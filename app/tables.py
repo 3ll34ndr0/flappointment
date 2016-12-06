058d207405bdbfde8aa94519045c50f035be4389
locomalo <elleandro@gmail.com>
2016-12-06 20:37:04 -0300

    mm

--- app/tables.py
+++ app/tables.py
@@ -1,4 +1,4 @@
-app import db
+from app import db
 from datetime import datetime, timedelta
 class User(db.Model):
     __tablename__ = "users"
@@ -89,7 +89,7 @@ class Appointment(db.Model):
         self.initHour    = initHour
         self.endHour     = endHour
     def __repr__(self):
-        return "*{}*: {}".format(self.activity.name, self.initHour)
+        return "*{}*: {}>".format(self.activity.name, self.initHour)
     #TODO: Lear how to show the name of the id_activity..."
 
 
